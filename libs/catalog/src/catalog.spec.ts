f97a2863465437c4c88bc4f2d6b36b0eae8257c4
mikeeus <itsmikias@gmail.com>
2017-11-26 14:22:22 +0300

    Generate home and catalog modules

---
+++ libs/catalog/src/catalog.spec.ts
@@ -0,0 +1,7 @@
+import { Catalog } from './catalog';
+
+describe('Catalog', () => {
+  it('should work', () => {
+    expect(new Catalog()).toBeDefined();
+  });
+});
---
+++ libs/home/src/home.spec.ts
@@ -0,0 +1,7 @@
+import { Home } from './home';
+
+describe('Home', () => {
+  it('should work', () => {
+    expect(new Home()).toBeDefined();
+  });
+});
