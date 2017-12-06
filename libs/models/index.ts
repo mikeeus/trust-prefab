68caf8fef018eb60c9b962e1670cf9e6c2641821
mikeeus <itsmikias@gmail.com>
2017-12-06 21:33:21 +0300

    Create models lib and house model

---
+++ libs/models/index.ts
@@ -0,0 +1,2 @@
+export { Models } from './src/models';
+export { House } from './src/house.model';
---
+++ libs/models/src/house.model.ts
@@ -0,0 +1,9 @@
+export class House {
+  name: string;
+  images: { main: string, sub: string, plan: string };
+  area: number;
+  model: string;
+  short_desc: string;
+  classification: string;
+  description: string;
+}
