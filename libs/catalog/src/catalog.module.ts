fa1a84de6ed05a6660728f39bae9193f3806d953
mikeeus <itsmikias@gmail.com>
2017-12-06 21:50:08 +0300

    lint

--- libs/catalog/src/catalog.module.ts
+++ libs/catalog/src/catalog.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatSidenavModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatCardModule, MatButtonModule, MatListModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CatalogComponent } from './catalog/catalog.component';
 import { HouseDetailComponent } from './house-detail/house-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '',
+  {
+    path: '',
     component: LayoutComponent,
     children: [
       { path: '', component: CatalogComponent },
@@ -23,6 +24,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
