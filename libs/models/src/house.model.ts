9f6cde879a5b2e59a926e5f87fc2f0155c187060
mikeeus <itsmikias@gmail.com>
2017-12-09 13:26:21 +0300

    Add instantiation and floorplans getter

--- libs/models/src/house.model.ts
+++ libs/models/src/house.model.ts
@@ -6,4 +6,33 @@ export class House {
   short_desc: string;
   classification: string;
   description: string;
+  floors: number;
+
+  constructor(house: {
+    name: string;
+    images: { main: string; sub: string; plan: string };
+    area: number;
+    model: string;
+    short_desc: string;
+    classification: string;
+    description: string;
+    floors?: 2;
+  }) {
+    this.name = house.name;
+    this.images = house.images;
+    this.area = house.area;
+    this.model = house.model;
+    this.short_desc = house.short_desc;
+    this.classification = house.classification;
+    this.description = house.description;
+    this.floors = house.floors || 1;
+  }
+
+  get floorplans() {
+    if (this.floors === 1) {
+      return [`${this.name}.jpg`];
+    } else {
+      return [`${this.name}_0.jpg`, `${this.name}_1.jpg`];
+    }
+  }
 }
